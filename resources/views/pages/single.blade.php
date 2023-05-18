308178e56214217cb5d74216914e1bb029ba86a2
Daniel Bellfield <dnlbellfield@gmail.com>
2023-05-18 16:23:23 -0700

    results page working

--- resources/views/pages/single.blade.php
+++ resources/views/pages/single.blade.php
@@ -11,24 +11,11 @@
     </p>
     <h2 class="mb-20 font-heading font-semibold text-center text-6xl sm:text-7xl text-white">Star Wars
     </h2>
-    <div class="flex flex-wrap -m-3">
-      <div class="w-full md:w-1/2 xl:w-1/4 p-3">
+    <div class="flex flex-wrap -m-3"></div> 
+    <!-- show poster  -->
+    <div class="w-full md:w-1/2 xl:w-1/4 p-3">
         <div class="flex flex-col justify-end h-full relative bg-gradient-cyan overflow-hidden rounded-10">
           <img class="mx-auto w-full" src="gradia-assets/images/teams/avatar-xl.png" alt="">
-          <div class="absolute bottom-0 left-0 w-full p-2.5">
-            <div class="p-5 w-full bg-white rounded-md">
-              <h2 class="font-heading font-bold text-lg text-gray-900">Star Wars
-              </h2>
-              <p class="text-sm text-gray-600">Sci-fi
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="content-area">
-          <h2> Star Wars</h2>
-        </div>
-
-      </div>
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,12 +20,63 @@ Route::get('/', function () {
 });
 
 Route::get('/search', function (Request $request) {
-  return $request->input('query');
-  return view('pages.results');
+  // return $request->input('query');
+  $query = $request->input('query');
+  $request_url = "https://api.watchmode.com/v1/autocomplete-search/?apiKey=" . env('WATCHMODE_KEY') . "&search_field=name&search_value=" . $query;
+  
+
+  $response = Http::get($request_url);
+
+  $results = $response->json();
+  $results = $results["results"];
+  return view('pages.results', [
+    "results" => $results,
+    "query" => ucwords($query)
+  ]);
 });
 
-Route::get('/movie', function () {
-  return view('pages.single');
+Route::get('/{type}/{id}', function (Request $request, $type, $id) {
+  // return $request->input('query');
+  // $query = $request->input('query');
+  $request_url = "https://api.watchmode.com/v1/title/345534/details/?apiKey=" . env('WATCHMODE_KEY') . "&append_to_response=sources";
+  
+
+  $response = Http::get($request_url);
+
+  $results = $response->json();
+  // return $results['sources'];
+  $rent_sources =[];
+  $buy_sources =[];
+  $stream_sources =[];
+  $free_sources =[];
+  
+  foreach ($results['sources'] as $item) {
+      if ($item['type'] == 'rent') {
+        $rent_sources[] = $item;
+      }
+      if ($item['type'] == 'buy') {
+        $buy_sources[] = $item;
+      }
+      if ($item['type'] == 'sub') {
+        $stream_sources[] = $item;
+      }
+      if ($item['type'] == 'free') {
+        $free_sources[] = $item;
+      }
+  }
+  // return $rent_sources;
+  return view('pages.single', [
+    "data" => $results,
+    "rent_sources" => $rent_sources,
+    "buy_sources" => $buy_sources,
+    "stream_sources" => $stream_sources,
+    "free_sources" => $free_sources,
+  ]);
 });
+
+
+// Route::get('/movie', function () {
+//   return view('pages.single');
+// });
 
 
