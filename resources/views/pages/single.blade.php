d9ee7f180f30c15d361faaa05a963c5c9845d1d6
Daniel Bellfield <dnlbellfield@gmail.com>
2023-05-18 18:23:11 -0700

    movie detail sources

--- resources/views/pages/single.blade.php
+++ resources/views/pages/single.blade.php
@@ -5,12 +5,7 @@
 @section('content')
 <section class="relative pt-28 pb-36 bg-black overflow-hidden">
   <div class="container mx-auto px-4">
-    <p
-      class="mb-6 max-w-max mx-auto text-center text-transparent bg-clip-text bg-gradient-cyan2 font-heading text-xs uppercase font-semibold tracking-px">
-      HERE'S YOUR RESULTS
-    </p>
-    <h2 class="mb-20 font-heading font-semibold text-center text-6xl sm:text-7xl text-white">Star Wars
-    </h2>
+
     <div class="flex flex-wrap -m-3">
     <!-- show poster  -->
     <div class="w-full md:w-/4 xl:w-1/4 p-3">
@@ -18,6 +13,7 @@
           <img class="mx-auto w-full" src="{{$data['poster']}}" alt="">
         </div>
     </div>
+
     <div class="w-full md:w-3/4 xl:w-3/4 p-3 text-white">
       <h2 class="text-5xl mb-6 max-w-max mx-auto text-transparent bg-clip-text bg-gradient-cyan2 font-heading text-xs uppercase font-semibold tracking-px">{{$data['title']}}</h2> 
       <div class="md:w-4/12 text-xl text-white">
@@ -26,9 +22,42 @@
       <span> {{$genre}}</span>
       @endforeach
     </div>
+    <div class="md:w-4/12 text-xl text-white">
+      <span class="font-bold">Release Date: </span> {{$data['release_date']}}
+    </div>
+    <section class="services">
+      <div class="py-4 flex">
+        <div class="md:w-3/12">
+          <h3 class="text-2xl text-white font-bold">Stream</h3>
+          <ul>
+            @foreach ($stream_sources as $item )
+            <li><a href="{{$item['web_url']}}">{{$item['name']}} {{$item['price']}} </a></li>
+              
+            @endforeach
+          </ul>
+        </div>
+        <div class="md:w-3/12">
+          <h3 class="text-2xl text-white font-bold">Rent</h3>
+          <ul>
+            @foreach ($rent_sources as $item )
+            <li><a href="{{$item['web_url']}}">{{$item['name']}} {{$item['price']}} </a></li>
+              
+            @endforeach
+          </ul>
+        </div>
+        <div class="md:w-3/12">
+          <h3 class="text-2xl text-white font-bold">Buy</h3>
+          <ul>
+            @foreach ($buy_sources as $item )
+            <li><a href="{{$item['web_url']}}">{{$item['name']}} {{$item['price']}} </a></li>
+              
+            @endforeach
+          </ul>
+        </div>
+      </div>
+    </section>
     
-
-
+    
   </div>
 </div> 
 </section>
